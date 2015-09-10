71aac9a622ab667c5158c51e0f9bf5b8f73966ee
Степан Дибров <dibrovsd@gmail.com>
2015-09-10 23:54:44 +0300

    ---

--- sql/asan/scripts/update_bso_for_test.sql
+++ sql/asan/scripts/update_bso_for_test.sql
@@ -1,4 +1,7 @@
 -- Проставить в БСО владельцем юзера (чтоб тестировать)
 
 update docflow_documentevent1
-set user_responsible_id = 1
+set user_responsible_id = 28;
+
+update docflow_document1
+set inscompany_id = 11;
